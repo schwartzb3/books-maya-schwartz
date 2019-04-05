3035be9e5539896606864b8dbb4e7d016f19a903
schwartzb <schwartzb@carleton.edu>
2019-04-04 19:34:15 -0500

    Fixed a string literal bug

--- books1.py
+++ books1.py
@@ -6,7 +6,7 @@ import re
 def readFile(file, action):
     lines = []
     x = 0
-    if action == 'authors’:
+    if action == "authors":
         x = 2
     try:
         with open(file, newline='') as f:
